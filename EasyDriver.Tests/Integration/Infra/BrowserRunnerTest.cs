73144c7322c27fc8f07a14938ba7751c25715594
Piotr Kluz <piotr.kluz.external@worldline.com>
2024-07-29 00:46:51 +0200

    Add Chromium proxy functionality

---
+++ EasyDriver.Tests/Integration/Infra/BrowserRunnerTest.cs
@@ -0,0 +1,27 @@
+﻿using Comfast.EasyDriver.Se.Finder;
+using Comfast.EasyDriver.Se.Infra.Browser;
+using EasyDriver.Tests.Util;
+using FluentAssertions;
+using Xunit.Abstractions;
+
+namespace EasyDriver.Tests.Integration.Infra;
+
+public class BrowserRunnerTest : IntegrationBase {
+    public BrowserRunnerTest(ITestOutputHelper output, IntegrationFixture fix) : base(output, fix) { }
+
+    // [Fact]
+    [Fact(Skip = "Unstable, dependent on external proxy provider")]
+    void RunWithProxy() {
+        // find some proxy here https://free-proxy-list.net/
+        Uri proxyUrl = new Uri("http://155.94.241.134:3128");
+
+        var conf = Configuration.DriverConfig;
+        conf.Headless = false;
+        conf.ProxyUrl = proxyUrl.OriginalString;
+
+        var driver = new BrowserRunner(conf).RunNewBrowser();
+        driver.Url = "http://ident.me";
+
+        driver.Find("body").Text.Should().Be(proxyUrl.Host);
+    }
+}
